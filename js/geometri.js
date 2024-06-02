11f51d2e1f0645a8f3b2127480bbd30e671a33d1
shahin2525 <shahin451999@gmail.com>
2024-06-02 01:28:38 -0700

    parallogram calculate

--- js/geometri.js
+++ js/geometri.js
@@ -37,3 +37,47 @@ document.getElementById("rectangle-btn").addEventListener("click", () => {
   const newTriangleArea = previousTriangleAreaNumber + rectangleArea;
   previousTriangleAreaText.innerText = newTriangleArea;
 });
+
+document.getElementById("parallelogram-btn").addEventListener("click", () => {
+  const parallelogramBaseInput = getInputField("parallelogram-base-input");
+
+  const parallelogramHeightInput = getInputField("parallelogram-height-input");
+  console.log(parallelogramHeightInput);
+
+  const previousParallelogramArea = getPreviousTextField("parallelogram-area");
+
+  const newParallelogramArea =
+    parallelogramBaseInput * parallelogramHeightInput;
+  previousParallelogramArea.innerText = newParallelogramArea;
+
+  // const rectangleWidthInput = document.getElementById("rectangle-width-input");
+  // const rectangleWidthInputString = rectangleWidthInput.value;
+  // const rectangleWidthInputNumber = parseFloat(rectangleWidthInputString);
+  // console.log(rectangleWidthInputNumber);
+  // const rectangleInputLength = document.getElementById(
+  //   "rectangle-length-input"
+  // );
+  // const rectangleInputLengthString = rectangleInputLength.value;
+  // const rectangleInputLengthNumber = parseFloat(rectangleInputLengthString);
+  // console.log(rectangleInputLengthNumber);
+  // const rectangleArea = rectangleWidthInputNumber * rectangleInputLengthNumber;
+  // const previousTriangleAreaText = document.getElementById("rectangle-area");
+  // const previousTriangleAreaString = previousTriangleAreaText.innerText;
+  // const previousTriangleAreaNumber = parseFloat(previousTriangleAreaString);
+  // const newTriangleArea = previousTriangleAreaNumber + rectangleArea;
+  // previousTriangleAreaText.innerText = newTriangleArea;
+});
+
+const getInputField = (fieldId) => {
+  const inputValueField = document.getElementById(fieldId);
+  const inputValueString = inputValueField.value;
+
+  const inputValueNumber = parseFloat(inputValueString);
+  return inputValueNumber;
+};
+const getPreviousTextField = (fieldId) => {
+  const previousFieldText = document.getElementById(fieldId);
+  const previousTextString = previousFieldText.innerText;
+  const previousTextNumber = parseFloat(previousTextString);
+  return previousTextNumber;
+};
